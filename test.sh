d3de65c60094369408951db8ab0d05117601fb7d
antham <hamonanth@gmail.com>
2016-10-10 00:02:30 +0200

    ref(test.sh) : add verbose flag

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo "" > coverage.txt
 
 for d in $( find ./* -maxdepth 10 ! -path "./vendor*" ! -path "./.git*" -type d); do
     if ls $d/*.go &> /dev/null; then
-        go test -race -coverprofile=profile.out -covermode=atomic $d
+        go test -v -race -coverprofile=profile.out -covermode=atomic $d
         if [ -f profile.out ]; then
             cat profile.out >> coverage.txt
             rm profile.out
