e080f69f94a3ea05d02203d6f348781a89b6d714
antham <hamonanth@gmail.com>
2016-10-16 13:00:32 +0200

    fix(.travis.yml) : add libgit2

--- install-libgit2.sh
+++ install-libgit2.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-cd "${HOME}"
-LG2VER="0.24.0"
-wget -O libgit2-${LG2VER}.tar.gz https://github.com/libgit2/libgit2/archive/v${LG2VER}.tar.gz
-tar -xzvf libgit2-${LG2VER}.tar.gz
-cd libgit2-${LG2VER} && mkdir build && cd build
-cmake -DTHREADSAFE=ON -DBUILD_CLAR=OFF -DCMAKE_BUILD_TYPE="RelWithDebInfo" .. && make && sudo make install
-sudo ldconfig
-cd "${TRAVIS_BUILD_DIR}"
+cd "$GOPATH"/src/github.com/antham/gommit/vendor/github.com/libgit2/git2go/ || exit
+
+git fetch --all
+git checkout -b next --track origin/next
+git submodule update --init
+make install
