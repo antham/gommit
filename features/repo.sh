8289152a8f157cac59fb2cd671748a47e1d7f9b2
antham <hamonanth@gmail.com>
2016-11-06 23:33:52 +0100

    test(features) : set git config

--- features/repo.sh
+++ features/repo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -z ${TRAVIS+x} ];
+then
+    git config --global user.name "whatever";
+    git config --global user.email "whatever@example.com";
+fi
+
+# Configure name
+
 # Init
 rm -rf test > /dev/null
 git init test
