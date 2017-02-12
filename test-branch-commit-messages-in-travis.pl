a954d4268ad9862b98b2b6104a785cf047fa124a
antham <hamonanth@gmail.com>
2017-02-12 11:11:08 +0100

    fix(travis-script) : make it works on master
    * remove useless stuffs

--- test-branch-commit-messages-in-travis.pl
+++ test-branch-commit-messages-in-travis.pl
@@ -1,17 +1,5 @@
 #!/bin/perl
 
-my $branch = '';
-
-if ($ENV{'TRAVIS_PULL_REQUEST'} eq 'false') {
-    $branch = `$ENV{TRAVIS_BRANCH}`;
-} else {
-    $branch = `$ENV{TRAVIS_PULL_REQUEST_BRANCH}`;
-}
-
-if ($branch eq 'master') {
-    exit 0;
-}
-
 `git ls-remote origin master` =~ /([a-f0-9]{40})/;
 
 my $refHead = `git rev-parse HEAD`;
@@ -19,6 +7,10 @@ my $refTail = $1;
 
 chomp($refHead);
 chomp($refTail);
+
+if ($refHead eq $refTail) {
+    exit 0;
+}
 
 system "gommit check range $refTail $refHead";
 
