bc0dd74e9414ddba21c456f5251c9df371472a31
anthony <hamon.anth@gmail.com>
2025-02-09 18:22:51 +0100

    Upgrade library

--- gommit/gommit.go
+++ gommit/gommit.go
@@ -5,8 +5,8 @@ import (
 	"strings"
 
 	"github.com/dlclark/regexp2"
-	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 
 	"github.com/antham/gommit/reference"
 )
